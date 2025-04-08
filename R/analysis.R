34f6b8a3556498682c33e85fd6d8ba39289b1819
Federico P. Cortese <cortese.federico@live.it>
2025-04-08 17:22:36 +0200

    mods

--- R/analysis.R
+++ R/analysis.R
@@ -2,6 +2,7 @@ load("FEDRA_varsxclust_all.RData") # 27 variabili, ovvero più o meno tutti gli 
 load("FEDRA_varsxclust_k7.RData") # 7 variabili selezionate per rappresentare teoricamente costrutti ben distinti ed essenziali
 load("FEDRA_varsxclust_k12.RData") # 12 variabili, che rappresentano costrutti non così distinti ma comunque diversi e potenzialmente utili
 
-
+# In ogni file c'è una variabile IDK che rappresenta il soggetto e una variabile GRP che rappresenta il gruppo. 
+# I soggetti con GRP = EXC vanno esclusi.
 
 

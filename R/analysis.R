c0e22daf8870cb69b074ad8d649d906990725c76
Federico P. Cortese <cortese.federico@live.it>
2025-04-16 15:48:39 +0200

    mods

--- R/analysis.R
+++ R/analysis.R
@@ -11,7 +11,13 @@ data_features_all$GRP <- as.factor(data_features_all$GRP)
 unique(data_features_all$GRP)
 head(data_features_all)
 
+head(output_bart$BUGSoutput$sims.matrix[,1])
+plot(output_bart$BUGSoutput$sims.matrix[,1],type='l')
+hist(output_bart$BUGSoutput$sims.matrix[,1])
+
 # Beta
+library(ggplot2)
+x11()
 ggplot(data_features_all, aes(x = GRP, y = beta, fill = GRP)) +
   geom_boxplot(outlier.shape = NA, alpha = 0.7) +  # boxplot without showing outliers
   geom_jitter(width = 0.2, alpha = 0.6) +  # optional: individual points
@@ -22,6 +28,7 @@ ggplot(data_features_all, aes(x = GRP, y = beta, fill = GRP)) +
   theme(legend.position = "none")
 
 # Rho
+x11()
 ggplot(data_features_all, aes(x = GRP, y = rho, fill = GRP)) +
   geom_boxplot(outlier.shape = NA, alpha = 0.7) +  # boxplot without showing outliers
   geom_jitter(width = 0.2, alpha = 0.6) +  # optional: individual points
@@ -32,6 +39,7 @@ ggplot(data_features_all, aes(x = GRP, y = rho, fill = GRP)) +
   theme(legend.position = "none")
 
 # Arew
+x11()
 ggplot(data_features_all, aes(x = GRP, y = Arew, fill = GRP)) +
   geom_boxplot(outlier.shape = NA, alpha = 0.7) +  # boxplot without showing outliers
   geom_jitter(width = 0.2, alpha = 0.6) +  # optional: individual points
@@ -42,6 +50,7 @@ ggplot(data_features_all, aes(x = GRP, y = Arew, fill = GRP)) +
   theme(legend.position = "none")
 
 # Apun
+x11()
 ggplot(data_features_all, aes(x = GRP, y = Apun, fill = GRP)) +
   geom_boxplot(outlier.shape = NA, alpha = 0.7) +  # boxplot without showing outliers
   geom_jitter(width = 0.2, alpha = 0.6) +  # optional: individual points
@@ -52,6 +61,7 @@ ggplot(data_features_all, aes(x = GRP, y = Apun, fill = GRP)) +
   theme(legend.position = "none")
 
 # K
+x11()
 ggplot(data_features_all, aes(x = GRP, y = K, fill = GRP)) +
   geom_boxplot(outlier.shape = NA, alpha = 0.7) +  # boxplot without showing outliers
   geom_jitter(width = 0.2, alpha = 0.6) +  # optional: individual points
@@ -62,6 +72,7 @@ ggplot(data_features_all, aes(x = GRP, y = K, fill = GRP)) +
   theme(legend.position = "none")
 
 # betaF
+x11()
 ggplot(data_features_all, aes(x = GRP, y = betaF, fill = GRP)) +
   geom_boxplot(outlier.shape = NA, alpha = 0.7) +  # boxplot without showing outliers
   geom_jitter(width = 0.2, alpha = 0.6) +  # optional: individual points
@@ -72,6 +83,7 @@ ggplot(data_features_all, aes(x = GRP, y = betaF, fill = GRP)) +
   theme(legend.position = "none")
 
 # betaP
+x11()
 ggplot(data_features_all, aes(x = GRP, y = betaP, fill = GRP)) +
   geom_boxplot(outlier.shape = NA, alpha = 0.7) +  # boxplot without showing outliers
   geom_jitter(width = 0.2, alpha = 0.6) +  # optional: individual points
@@ -80,3 +92,22 @@ ggplot(data_features_all, aes(x = GRP, y = betaP, fill = GRP)) +
        x = "Group",
        y = "betaP") +
   theme(legend.position = "none")
+
+
+# (1) Considera tutte le variabili, se al clustering uniamo una procedura di variable selection 
+# eccetto le variabili con 'bart' (1) e 'iowa' (4) e ovviamente aggiungere beta, rho, Arew etc...
+# hard clustering con un algoritmo COSA
+
+# (2) Sparse k-means (hard) 
+
+# (3) Fuzzy k-means (soft): una prob. di appartenere a ciascun gruppo a ciascun paziente 
+# Da provare cosi' com'e' e magari fare PCA prima oppure combinare (pero non c'e' in letteratura, tipo fuzzy-COSA)
+
+# (4) Misture gaussiane (soft) con (tutte le variabili) o senza selezione delle variabili (usa solo le k7 in questo caso) 
+# oppure con trimming (opportuna gestione degli outlier), risultato ulteriore: rilevazione delle anomalie
+
+# N.B. Trimming importante (trimmed-COSA?)
+
+
+
+
